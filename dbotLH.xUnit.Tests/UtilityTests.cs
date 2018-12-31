b82c3a436bffc00740e60bf7ca6661587b31a1f9
Naichiri <naichiri@gmail.com>
2018-12-31 19:44:38 +0100

    Add Unit Tests (xUnit)

---
+++ dbotLH.xUnit.Tests/UtilityTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace dbotLH.xUnit.Tests
+{
+    public class UtilityTests
+    {
+        [Fact]
+        public void TestName()
+        {
+        //Given
+        
+        //When
+        
+        //Then
+        }
+    }
+}
