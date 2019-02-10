494e46f72f325bf61fdd24648ab95e041b34714e
Naichiri <naichiri@gmail.com>
2019-02-10 13:09:42 +0100

    Update BotActivity.cs
    Removed unneeded private fields - code simplification.

--- FuyumiBot/Core/Entities/BotActivity.cs
+++ FuyumiBot/Core/Entities/BotActivity.cs
@@ -4,19 +4,8 @@ namespace FuyumiBot.Core.Entities
 {
     public class BotActivity : IActivity
     {
-        private string _name;
-        private ActivityType _type;
+        public string Name { get; set; }
 
-        public string Name
-        {
-            get => _name;
-            set => _name = value;
-        }
-
-        public ActivityType Type 
-        {
-            get => _type;
-            set => _type = value;
-        }
+        public ActivityType Type { get; set; }
     }
 }
