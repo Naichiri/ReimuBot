eab5b42c49813817ad2950f5bcfea4a240d38247
Naichiri <naichiri@gmail.com>
2019-03-10 15:10:48 +0100

    Remove test in LoggerTests.cs
    The code the test was checking was removed.

--- FuyumiBot.xUnit.Tests/LoggerTests.cs
+++ FuyumiBot.xUnit.Tests/LoggerTests.cs
@@ -5,14 +5,6 @@ namespace FuyumiBot.xUnit.Tests
     public class LoggerTests
     {
         [Fact]
-        public void WhenGivenNullThenThrowsExceptionTest()
-        {
-            var logger = Unity.Resolve<ILogger>();
-
-            Assert.NotNull(logger);
-            Assert.Throws<System.ArgumentException>(() => logger.Log(null));
-        }
-        [Fact]
         public void LogTest()
         {
             var expected = "log";
